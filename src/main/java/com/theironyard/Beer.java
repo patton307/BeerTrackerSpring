d0734d6deca5226d0dea1440cfd2c072ec52d497
landonkail <landon.kail@gmail.com>
2015-11-10 12:18:39 -0500

    initial commit

---
+++ src/main/java/com/theironyard/Beer.java
@@ -0,0 +1,19 @@
+package com.theironyard;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by landonkail on 11/10/15.
+ */
+@Entity
+public class Beer {
+    @Id
+    @GeneratedValue
+    Integer id;
+
+    String name;
+    String type;
+
+}
---
+++ src/main/java/com/theironyard/BeerRepository.java
@@ -0,0 +1,11 @@
+package com.theironyard;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by landonkail on 11/10/15.
+ */
+public interface BeerRepository extends CrudRepository<Beer, Integer> {
+}
+
+
